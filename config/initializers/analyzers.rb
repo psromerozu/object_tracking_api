9434101b98fc06b800c5424f5c16c4f71b8de9e1
Pablo Romero <promero@lumu.io>
2020-02-14 01:21:58 -0500

    Fixed initializers

--- config/initializers/analyzers.rb
+++ config/initializers/analyzers.rb
@@ -1,18 +1,6 @@
 require "tracker"
 
-SCREENSHOT_PATH  ="#{Rails.root}/public/screenshots"
-TRADEMARKS_PATH  ="#{Rails.root}/public/tracker/clients"
-LOG_FILE_PATH ="#{Rails.root}/log/track_api.log"
-API_FQDN = "http://tracker.pci.easysol.net"
 apikey = []
-
-if Rails.env.production?
-	PSQLSCHEMASUFFIX ="pci"
-else
-	PSQLSCHEMASUFFIX ="pcidev"
-end
-#create html & screenshots directories if they do not exist
-FileUtils.mkdir_p SCREENSHOT_PATH rescue nil
 
 #check if instance-key is in place
 File.open("api-key").each do | line |
@@ -27,5 +15,5 @@ else
 end
 
 #initialize Visual Analyzer
-Ttracker = Tracker.new
+#Ttracker = Tracker.new
 
